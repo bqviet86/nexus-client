b34b502eeaf14d87243052eb48588718d039e79a
bqviet86 <bqvct4211@gmail.com>
2024-08-02 18:39:42 +0700

    update vite.config.ts for server and preview

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,11 @@ import path from 'path'
 export default defineConfig({
     plugins: [react()],
     server: {
+        host: '0.0.0.0',
         port: 3000
     },
     preview: {
+        host: '0.0.0.0',
         port: 3000
     },
     css: {
